4b080a4b6ddbe88434f3eb2047bebe3c1817155b
Joko <kalle0045@gmail.com>
2024-07-24 14:45:02 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, makeStateKey, TransferState } from '@angular/core';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgFor } from '@angular/common';
+import { PageService } from './page.service'; // Import the service
 
 const POSTS_KEY = makeStateKey<any[]>('posts');
 
@@ -9,25 +10,28 @@ const POSTS_KEY = makeStateKey<any[]>('posts');
   standalone: true,
   imports: [NgFor, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PageService] // Provide the service
 })
 export class AppComponent implements OnInit {
   title = 'ng';
   posts: any[] = [];
 
-  constructor(private http: HttpClient, private transferState: TransferState) { }
+  constructor(private myService: PageService, private transferState: TransferState) { }
 
-  async ngOnInit(): Promise<void> {
+  ngOnInit(): void {
     this.posts = this.transferState.get(POSTS_KEY, null as any);
     if (!this.posts) {
-      try {
-        this.posts = await this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').toPromise() ?? [];
-        this.transferState.set(POSTS_KEY, this.posts);
-      } catch (error) {
-
-        console.error('Error fetching posts:', error);
-        // Handle error gracefully, e.g., display an error message to the user
-      }
+      this.myService.getPosts().subscribe({
+        next: (data) => {
+          this.posts = data;
+          this.transferState.set(POSTS_KEY, this.posts);
+        },
+        error: (error) => {
+          console.error('Error fetching posts:', error);
+          // Handle error gracefully, e.g., display an error message to the user
+        }
+      });
     }
   }
 }
